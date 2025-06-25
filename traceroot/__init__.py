dbd63ef84c07df8f5af7f2f492a8ffe6be317a25
Zecheng Zhang <zecheng@alumni.stanford.edu>
2025-06-25 12:32:45 -0700

    Simplify init by using yaml config

--- traceroot/__init__.py
+++ traceroot/__init__.py
@@ -2,6 +2,8 @@ from traceroot.logger import get_logger
 from traceroot.tracer import _initialize_tracing as init
 from traceroot.tracer import trace
 
+init()
+
 __all__ = [
     'init',
     'trace',
