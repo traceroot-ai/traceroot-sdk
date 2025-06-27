df0e11d496187287e97e467d51407f885c55becb
Zecheng Zhang <zecheng@alumni.stanford.edu>
2025-06-26 21:57:10 -0700

    Update version

--- traceroot/__init__.py
+++ traceroot/__init__.py
@@ -2,7 +2,7 @@ from traceroot.logger import get_logger
 from traceroot.tracer import _initialize_tracing as init
 from traceroot.tracer import trace
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 init()
 
