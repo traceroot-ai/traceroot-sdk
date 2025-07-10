5b1ff7b5502dc21f3d2d5325470a8d18ed1360c2
Zecheng Zhang <zecheng@alumni.stanford.edu>
2025-07-10 12:53:44 -0700

    Update

--- traceroot/logger.py
+++ traceroot/logger.py
@@ -154,7 +154,7 @@ class TraceRootLogger:
     def _fetch_aws_credentials(self) -> dict:
         """Fetch AWS credentials from the traceroot endpoint"""
         try:
-            url = "http://localhost:8000/verify/credentials"
+            url = "http://3.23.162.117:8000/verify/credentials"
             params = {"token": self.config.token}
             headers = {"Content-Type": "application/json"}
 
