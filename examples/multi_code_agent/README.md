901ca7cef20e1679e3fed9165e36991c1dac9683
Zecheng Zhang <zecheng@alumni.stanford.edu>
2025-07-31 01:11:40 -0700

    Minor update

--- examples/multi_code_agent/README.md
+++ examples/multi_code_agent/README.md
@@ -9,7 +9,19 @@ export OPENAI_API_KEY=your_api_key_here
 
 # Run
 
+To run the server, run the following command:
+
 ```bash
 cd ../../
-python examples/multi_agent/main.py
+python examples/multi_code_agent/simple_server.py
+```
+
+This will start a server on port 9999.
+
+## Test by Sending a Request
+
+```bash
+curl -X POST "http://localhost:9999/code" \
+        -H "Content-Type: application/json" \
+        -d '{"query": "Write a Python function to calculate fibonacci numbers"}'
 ```
--- examples/multi_code_agent/rest/main.py
+++ examples/multi_code_agent/rest/main.py
@@ -1,12 +1,12 @@
 import os
 from typing import Any, TypedDict
 
-from code_agent import create_code_agent
+from rest.code_agent import create_code_agent
 from dotenv import load_dotenv
-from execution_agent import create_execution_agent
+from rest.execution_agent import create_execution_agent
 from langgraph.graph import END, StateGraph
-from plan_agent import create_plan_agent
-from summarize_agent import create_summarize_agent
+from rest.plan_agent import create_plan_agent
+from rest.summarize_agent import create_summarize_agent
 
 import traceroot
 
